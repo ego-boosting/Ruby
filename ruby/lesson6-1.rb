eee6748f0de3b9b305f36c66c0313f5475ba1c5c
EC2 Default User <kazukipump7@gmail.com>
2022-05-25 06:04:24 +0000

    update_2

---
+++ ruby/lesson6-1.rb
@@ -0,0 +1,16 @@
+# puts "計算をはじめます
+# 2つの値を入力してください"
+# input_key = gets.to_i
+# input_key2 = gets.to_i
+# puts "計算結果を出力します"
+# puts input_key * input_key2
+# puts "計算を終了します"
+
+puts "計算をはじめます"
+puts "計算をはじめます"
+a = gets.to_i
+b = gets.to_i
+
+puts "計算結果を出力します。"
+puts"a * b = #{a * b}"
+puts "計算を終了させます"
